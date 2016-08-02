bfc71b7363dc988f573e827b1194ad53abc6ba14
UUood <juuood@gmail.com>
2016-08-02 02:20:43 +0000

    poster update

--- js/index.js
+++ js/index.js
@@ -7,9 +7,11 @@
 //Assign the var names
                     happyBG = f.select("#happyBG");
                     sadBG = f.select("#sadBG");
+                    lonelyBG = f.select("#lonelyBG");
 //Assign groups to var names
                     happy = f.select("#happy");
                     sad = f.select("#sad")
+                    lonely = f.select("#lonely")
 //Happy 1,1
                     happy.click(function() {
                         happy.toggleClass('on');
@@ -28,8 +30,20 @@
                         sadBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
                     }
 });
+//lonely 1,3
+                    lonely.click(function() {
+                        lonely.toggleClass('on');
+                        if (lonely.hasClass('on')) {
+						lonelyBG.animate({ fill: "#0000FF", stroke: "#686868" }, 150);
+                    }else{
+                        lonelyBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
+                    }
+});   
+   
+   
                     
 //Finally append the icon to iconDiv in the body
+                
 				s.append(f);
 				});		     
 			};
