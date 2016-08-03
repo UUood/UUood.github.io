590510aa3d1c562558f0b660b4416037d686c6f8
UUood <juuood@gmail.com>
2016-08-03 03:14:42 +0000

    image update

--- js/index.js
+++ js/index.js
@@ -11,12 +11,25 @@
                     lonelyBG = feelings.select("#lonelyBG");
                     boredBG = feelings.select("#boredBG");
                     confidentBG = feelings.select("#confidentBG");
+                    joyfulBG = feelings.select("#joyfulBG");
+                    discouragedBG = feelings.select("#discouragedBG");
+                    thoughfulBG = feelings.select("#thoughfulBG");
+                    guiltyBG = feelings.select("#guiltyBG");
+                    ashamedBG = feelings.select("#ashamedBG");
+
+                    
 //Assign groups to var names
                     happy = feelings.select("#happy");
                     sad = feelings.select("#sad");
                     lonely = feelings.select("#lonely");
                     bored = feelings.select("#bored");
                     confident = feelings.select("#confident");
+                    joyful = feelings.select("#joyful");    
+                    discouraged = feelings.select("#discouraged");
+                    thoughful = feelings.select("#thoughful");
+                    guilty = feelings.select("#guilty");
+                    ashamed = feelings.select("#ashamed");
+
 //Happy 1,1
                     happy.click(function() {
                         happy.toggleClass('on');
@@ -62,6 +75,43 @@
                         confidentBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
                     }
 });                     
+//joyful
+                    joyful.click(function() {
+                    joyful.toggleClass('on');
+                        if (joyful.hasClass('on')) {
+                            joyfulBG.animate({ fill: "#FF6600", stroke: "#686868" }, 150);
+                    }else{
+                        joyfulBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
+                    }
+                    });  
+//discouraged
+discouraged.click(function() {discouraged.toggleClass('on');
+                        if (discouraged.hasClass('on')) {discouragedBG.animate({ 
+                            fill: "#FF6600", stroke: "#686868" }, 150);
+                    }else{discouragedBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
+                    }
+});  
+/*//thoughful
+
+thoughful.click(function() {thoughful.toggleClass('on');
+                        if (thoughful.hasClass('on')) {thoughfulBG.animate({ fill: "#FF6600", stroke: "#686868" }, 150);
+                    }else{thoughfulBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
+                    }
+});*/  
+//guilty
+guilty.click(function() {guilty.toggleClass('on');
+                        if (guilty.hasClass('on')) {guiltyBG.animate({ fill: "#FF6600", stroke: "#686868" }, 150);
+                    }else{guiltyBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
+                    }
+});  
+//ashamed
+ashamed.click(function() {ashamed.toggleClass('on');
+                        if (ashamed.hasClass('on')) {ashamedBG.animate({ fill: "#FF6600", stroke: "#686868" }, 150);
+                    }else{ashamedBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
+                    }
+                    });  
+
+
 //Finally append the icon to iconDiv in the body
                 
 				s.append(f);
