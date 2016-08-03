0c1172f0a842700ff32e194497b41252ba447b76
UUood <juuood@gmail.com>
2016-08-03 02:37:03 +0000

    image update

--- js/index.js
+++ js/index.js
@@ -9,15 +9,19 @@
                     happyBG = feelings.select("#happyBG");
                     sadBG = feelings.select("#sadBG");
                     lonelyBG = feelings.select("#lonelyBG");
+                    boredBG = feelings.select("#boredBG");
+                    confidentBG = feelings.select("#confidentBG");
 //Assign groups to var names
                     happy = feelings.select("#happy");
-                    sad = feelings.select("#sad")
-                    lonely = feelings.select("#lonely")
+                    sad = feelings.select("#sad");
+                    lonely = feelings.select("#lonely");
+                    bored = feelings.select("#bored");
+                    confident = feelings.select("#confident");
 //Happy 1,1
                     happy.click(function() {
                         happy.toggleClass('on');
                         if (happy.hasClass('on')) {
-						happyBG.animate({ fill: "#FFFF00", stroke: "#686868" }, 150);
+						happyBG.animate({ fill: "#FF6600", stroke: "#686868" }, 150);
                     }else{
                         happyBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
                     }
@@ -40,9 +44,24 @@
                         lonelyBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
                     }
 });   
-   
-   
-                    
+//Bored 1,4
+                    bored.click(function() {
+                        bored.toggleClass('on');
+                        if (bored.hasClass('on')) {
+						boredBG.animate({ fill: "#66FF00", stroke: "#686868" }, 150);
+                    }else{
+                        boredBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
+                    }
+}); 
+//confident 1,5   
+                    confident.click(function() {
+                    confident.toggleClass('on');
+                        if (confident.hasClass('on')) {
+						confidentBG.animate({ fill: "#FF6600", stroke: "#686868" }, 150);
+                    }else{
+                        confidentBG.animate({ fill: "#FFFFFF", stroke: "#686868" }, 150);
+                    }
+});                     
 //Finally append the icon to iconDiv in the body
                 
 				s.append(f);
